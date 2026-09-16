b9570a22188dc9c309e6a3e1c6d25c0c8f97e665
Aditya Singh <akash.aditya001@gmail.com>
2026-09-16 18:43:00 +0530

    feat: add Docker Compose setup for MongoDB, backend, and frontend services

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
-
-
 const App = () => {
   return (
-    <div>
-      
+    // yahan 'bg-black' add kar diya hai
+    <div className="bg-black h-screen w-screen flex items-center justify-center"> 
+      <div className="bg-gray-500 h-1/2 w-1/2 text-center rounded-4xl "> 
+        <h1 className="mt-40">Hello Aditya 
+          <h2>heyy aditya you achive this now </h2>
+        </h1>
+      </div>
     </div>
   )
 }
